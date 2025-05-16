2ccdbe7208ff3302bdd1516e74bfccfcfb2d6735
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-16 16:14:27 +0900

    feat #4 : 글 상세 정보 조회 API 구현

---
+++ src/main/java/com/example/study_monster_back/board/controller/BoardController.java
@@ -0,0 +1,20 @@
+package com.example.study_monster_back.board.controller;
+
+import com.example.study_monster_back.board.dto.response.GetBoardResponseDto;
+import com.example.study_monster_back.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardService boardService;
+    @GetMapping("/{boardId}")
+    public ResponseEntity<GetBoardResponseDto> getBoard(
+        @PathVariable(value = "boardId") Long boardId
+    ) {
+        return ResponseEntity.ok(boardService.getBoard(boardId));
+    }
+}
