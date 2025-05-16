3067dfd08bd86fc55f48ad4c0acc04006484cf29
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-16 11:13:15 +0900

    init

---
+++ src/main/java/com/example/study_monster_back/StudyMonsterBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_monster_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMonsterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMonsterBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_monster_back/StudyMonsterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_monster_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMonsterBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
