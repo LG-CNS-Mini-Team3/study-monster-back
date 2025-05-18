075ad97602734efff7482e3a70793b38a84ad78d
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-19 01:22:38 +0900

    feat #7: dto 구현

--- src/main/java/com/example/study_monster_back/group/dto/StudyGroupResponseDTO.java
+++ src/main/java/com/example/study_monster_back/group/dto/StudyGroupResponseDTO.java
@@ -1,5 +1,29 @@
 package com.example.study_monster_back.group.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudyGroupResponseDTO {
+
+    private Long id;
+    private String name;
+    List<String> tags;
+    private LocalDateTime created_at;
+    private String description;
+    private Integer limit_members;
+    private LocalDateTime deadline;
+    private String status;
+    private int current;
+    private String nickname;
+    private String profileImage;
+
+    
     
 }
