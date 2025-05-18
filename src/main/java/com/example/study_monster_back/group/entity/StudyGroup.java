6f4890509ef02fa307070febff3bdceba75ff8ef
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-19 01:24:30 +0900

    feat #7: Controller(스터디그룹 리스트 정보 조회) 구현

--- src/main/java/com/example/study_monster_back/group/entity/StudyGroup.java
+++ src/main/java/com/example/study_monster_back/group/entity/StudyGroup.java
@@ -1,12 +1,8 @@
 package com.example.study_monster_back.group.entity;
 
-
 import java.time.LocalDateTime;
-
 import org.springframework.data.annotation.CreatedDate;
-
 import com.example.study_monster_back.user.entity.User;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,9 +16,8 @@ public class StudyGroup{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;
     @CreatedDate
+    private String name;
     private LocalDateTime created_at;
     private String description;
     private Integer limit_members;
