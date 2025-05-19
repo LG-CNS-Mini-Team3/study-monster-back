e2f3e01e158514fbdf8fe894490a7133395cf3a3
seohee-P <p03061180@gmail.com>
2025-05-19 14:42:26 +0900

    refactor #5: @CrossOrigin 메서드 레벨에서 컨트롤러 레벨로 이동

--- src/main/java/com/example/study_monster_back/board/controller/BoardController.java
+++ src/main/java/com/example/study_monster_back/board/controller/BoardController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RequiredArgsConstructor
 @Tag(name = "Board", description = "게시글 관련 API")
 @RequestMapping("/api/boards")
@@ -18,13 +19,11 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @CrossOrigin
     @PostMapping
     @Operation(summary = "게시글 작성",
             description = "게시글을 작성하고, 해당 게시글의 해시태그도 소문자로 변환하여 저장합니다.")
     public ResponseEntity<CreateBoardResponseDto> createBoard(@Valid @RequestBody CreateBoardRequestDto boardRequestDto) {
         // TODO: 추후에 @AuthenticationPrincipal로 유저 정보 가져올 예정
-        //  security에서 cors 설정할 수 있으므로 임시로 @CrossOrigin 사용
         CreateBoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto);
         return ResponseEntity.ok(boardResponseDto);
     }
