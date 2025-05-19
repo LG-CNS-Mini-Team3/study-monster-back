723a2037990d391d598f6c1cc9e5425338fb24ab
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-19 11:19:06 +0900

    chore #7: 불필요한 코드 삭제

--- src/main/java/com/example/study_monster_back/group/service/StudyGroupService.java
+++ src/main/java/com/example/study_monster_back/group/service/StudyGroupService.java
@@ -1,7 +1,6 @@
 package com.example.study_monster_back.group.service;
 
 import java.util.List;
-
 import com.example.study_monster_back.group.dto.StudyGroupResponseDTO;
 
 public interface StudyGroupService {
--- src/main/java/com/example/study_monster_back/group/service/StudyGroupServiceImpl.java
+++ src/main/java/com/example/study_monster_back/group/service/StudyGroupServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.study_monster_back.group.service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
