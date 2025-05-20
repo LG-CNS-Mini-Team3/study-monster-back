6bea7a79ec49266b735560ab28bdc545e43a2c5d
seohee-P <p03061180@gmail.com>
2025-05-21 02:52:37 +0900

    feat #22: 게시글 수정 관련 요청, 응답 Dto 생성

---
+++ src/main/java/com/example/study_monster_back/board/dto/request/UpdateBoardRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.study_monster_back.board.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBoardRequestDto {
+
+    @NotBlank(message = "제목은 필수입니다")
+    private String title;
+
+    @NotBlank(message = "내용은 필수입니다")
+    private String content;
+
+    private Long userId;
+
+    private List<String> tags;
+}
