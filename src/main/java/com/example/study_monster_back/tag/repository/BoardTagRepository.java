6de0f9265a53bfed6a428883589f5d4e4309ecdb
seohee-P <p03061180@gmail.com>
2025-05-21 02:28:39 +0900

    chore #22: @Repository 어노테이션 추가

--- src/main/java/com/example/study_monster_back/tag/repository/BoardTagRepository.java
+++ src/main/java/com/example/study_monster_back/tag/repository/BoardTagRepository.java
@@ -2,6 +2,8 @@ package com.example.study_monster_back.tag.repository;
 
 import com.example.study_monster_back.tag.entity.BoardTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardTagRepository extends JpaRepository<BoardTag, Long> {
 }
