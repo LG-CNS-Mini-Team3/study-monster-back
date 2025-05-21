9c158dd79fd07b38b2eb2242f9feacd85db4916d
jangseungwon <jangseungwon9819@gmail.com>
2025-05-21 13:41:45 +0900

    feat/#24/로그인 로그아웃 구현

---
+++ src/main/java/com/example/study_monster_back/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.example.study_monster_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.study_monster_back.jwt.JwtAuthenticationFilter;
+import com.example.study_monster_back.jwt.JwtTokenProvider;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider,
+            UserDetailsService userDetailsService)
+            throws Exception {
+        http.cors(withDefaults())
+                .csrf(csrf -> {
+                    csrf.disable();
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/logout", "/auth/update","/auth/delete","auth/user").authenticated()
+                        .requestMatchers("/auth/register").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().permitAll())
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userDetailsService),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+}
+}
---
+++ src/main/java/com/example/study_monster_back/user/service/RegisterService.java
@@ -0,0 +1,47 @@
+package com.example.study_monster_back.user.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.study_monster_back.user.dto.request.RegisterRequestDto;
+import com.example.study_monster_back.user.entity.User;
+import com.example.study_monster_back.user.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Transactional
+@Slf4j
+@Service
+public class RegisterService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void RegisterUser(RegisterRequestDto registerRequestDto) {
+        if (userRepository.existsByEmail(registerRequestDto.getEmail())) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+        if (userRepository.existsByNickname(registerRequestDto.getNickname())) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+        User user = new User();
+        user.setEmail(registerRequestDto.getEmail());
+        user.setName(registerRequestDto.getName());
+        user.setNickname(registerRequestDto.getNickname());
+        user.setPhone_number(registerRequestDto.getPhone_number());
+        String rawPwd = registerRequestDto.getOriginPwd();
+        String checkPwd = registerRequestDto.getCheckPwd();
+        if (rawPwd.equals(checkPwd)) {
+            String encryptPwd = bCryptPasswordEncoder.encode(rawPwd);
+            user.setPwd(encryptPwd);
+        }
+        else{
+            throw new IllegalArgumentException("유효하지 않은 비밀번호입니다.");
+        }
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+    }
+}
