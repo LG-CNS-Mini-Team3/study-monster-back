0bac84b40cdc7b3445cd65a85b0dd9147bd22f2e
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2025-05-21 11:06:21 +0900

    feat #9 : OpenAI callAPI 메서드 반환 타입 확장성 개선

--- src/main/java/com/example/study_monster_back/openAi/service/OpenAiServiceImpl.java
+++ src/main/java/com/example/study_monster_back/openAi/service/OpenAiServiceImpl.java
@@ -6,16 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import static com.example.study_monster_back.openAi.service.AiMessageRole.SYSTEM;
-import static com.example.study_monster_back.openAi.service.AiMessageRole.USER;
-import static com.example.study_monster_back.openAi.service.AiPrompt.*;
+import static com.example.study_monster_back.openAi.service.AiPrompt.STUDY_FEEDBACK_SYSTEM;
+import static com.example.study_monster_back.openAi.service.AiPrompt.STUDY_FEEDBACK_USER;
 
 @Slf4j
 @Service
@@ -25,7 +26,6 @@ public class OpenAiServiceImpl implements OpenAiService {
     private String openaiKey;
     @Value("${ai.openai.endpoint.chat-completions}")
     private String OPEN_AI_END_POINT;
-    private final ObjectMapper objectMapper;
 
     private RestClient getOpenAiClient() {
         return RestClient.create().mutate()
@@ -35,82 +35,100 @@ public class OpenAiServiceImpl implements OpenAiService {
             .build();
     }
 
-    private <T> T callApi(OpenAiRequest openAiRequest, Class<T> responseType) {
-        ParameterizedTypeReference<OpenAiResponse<T>> typeRef = new ParameterizedTypeReference<>() {
-        };
-        ResponseEntity<OpenAiResponse<T>> responseEntity = getOpenAiClient().post().body(openAiRequest).retrieve()
+    public <T> T callApi(String systemMessage, String userMessage, Class<T> responseType) {
+        List<OpenAiMessage> messages = List.of(
+            OpenAiMessage.builder().role("system").content(systemMessage).build(),
+            OpenAiMessage.builder().role("user").content(userMessage).build()
+        );
+        boolean wrapperOrString = WRAPPER_OR_STRING_TYPES.contains(responseType);
+        OpenAiRequest openAiRequest = OpenAiRequest.builder()
+            .messages(messages)
+            .response_format(
+                wrapperOrString ? null :
+                    getArticleFeedbackResponseFormatWithAnnotation(responseType.getSimpleName(), extractProperties(responseType))
+            )
+            .build();
+        OpenAiResponse openAiResponse = getOpenAiClient().post().body(openAiRequest).retrieve()
             .onStatus((status) -> {
                 if (status.getStatusCode().isError()) {
-                    log.error(String.format("Open AI 통신 에러 : %s %s", status.getStatusCode().value(), status.getStatusText()));
+                    System.out.println(String.format("Open AI 통신 에러 : %s %s", status.getStatusCode().value(), status.getStatusText()));
                     throw new RuntimeException("Open AI 와의 통신에 실패 했습니다.");
                 }
                 return false;
-            }).toEntity(typeRef);
-
-        Object content = responseEntity.getBody().getChoices().get(0).getMessage().getContent();
-        if (responseType.isInstance(content)) {
-            return responseType.cast(content);
-        } else if (content instanceof String str) {
-            try {
-                return objectMapper.readValue(str, responseType);
-            } catch (JsonProcessingException e) {
-                log.error(String.format("타입 캐스팅 실패 : %s", e.getMessage()));
-                throw new RuntimeException("타입 캐스팅 실패");
-            }
-        } else {
-            throw new RuntimeException("타입 캐스팅 실패");
+            }).body(OpenAiResponse.class);
+        if (wrapperOrString) {
+            return responseType.cast(openAiResponse.getChoices().get(0).getMessage().getContent());
+        }
+        try {
+            return (new ObjectMapper()).readValue(openAiResponse.getChoices().get(0).getMessage().getContent(), responseType);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("반환 json 형식 오류");
         }
     }
 
     public String getPlainAnswer(String systemMessage, String userMessage) {
-        OpenAiRequest openAiRequest = OpenAiRequest.builder()
-            .messages(getPromptedMessages(systemMessage, userMessage))
-            .build();
-        return callApi(openAiRequest, String.class);
+        return callApi(systemMessage, userMessage, String.class);
     }
 
     public OpenAiStudyFeedbackResponse getStudyFeedback(String title, String article) {
         String systemMessage = STUDY_FEEDBACK_SYSTEM.getPrompt();
         String userMessage = String.format(STUDY_FEEDBACK_USER.getPrompt(), title, article);
-        OpenAiRequest<StudyFeedbackProperties> openAiRequest = OpenAiRequest.<StudyFeedbackProperties>builder()
-            .messages(getPromptedMessages(systemMessage, userMessage))
-            .response_format(getStudyFeedbackResponseFormat())
-            .build();
-        return callApi(openAiRequest, OpenAiStudyFeedbackResponse.class);
+        return callApi(systemMessage, userMessage, OpenAiStudyFeedbackResponse.class);
     }
 
-    private List<OpenAiMessage> getPromptedMessages(String systemMessage, String userMessage) {
-        return List.of(
-            OpenAiMessage.builder().role(SYSTEM.getRole()).content(systemMessage).build(),
-            OpenAiMessage.builder().role(USER.getRole()).content(userMessage).build()
+    private static final Set<Class> WRAPPER_OR_STRING_TYPES = Set.of(
+        Byte.class, Short.class, Integer.class, Long.class,
+        Float.class, Double.class, Character.class, Boolean.class, String.class
+    );
+
+    private <T> Map<String, Map<String, String>> extractProperties(Class<T> responseType) {
+        Map<String, Map<String, String>> properties = new HashMap<>();
+        Field[] fields = responseType.getDeclaredFields();
+        for (Field field : fields) {
+            OpenAiSchema annotation = field.getAnnotation(OpenAiSchema.class);
+            String type;
+            if (field.getType().equals(Integer.class)) {
+                type = "integer";
+            } else if (Number.class.isAssignableFrom(field.getType())) { // 숫자
+                type = "number";
+            } else if (field.getType().equals(String.class)) {
+                type = "string";
+            } else if (field.getType().equals(Boolean.class)) {
+                type = "boolean";
+            } else {
+                type = "string";
+            }
+            properties.put(field.getName(), Map.of(
+                "type", type,
+                "description", (annotation == null ? field.getName() : annotation.description())
+            ));
+        }
+        return properties;
+    }
+
+    private Map getArticleFeedbackResponseFormatWithAnnotation(String name, Map<String, Map<String, String>> properties) {
+        return Map.of(
+            "type", "json_schema",
+            "json_schema", Map.of(
+                "name", name,
+                "schema", Map.of(
+                    "type", "object",
+                    "properties", properties,
+                    "required", properties.keySet(),
+                    "additionalProperties", false
+                )
+            )
         );
-    }
-
-    private OpenAiResponseFormat<StudyFeedbackProperties> getStudyFeedbackResponseFormat() {
-        return OpenAiResponseFormat.<StudyFeedbackProperties>builder()
-            .json_schema(JsonSchema.<StudyFeedbackProperties>builder()
-                .schema(Schema.<StudyFeedbackProperties>builder()
-                    .properties(StudyFeedbackProperties.builder()
-                        .feedback(PropertyType.builder()
-                            .description(STUDY_FEEDBACK_PROPERTY_DESCRIPTION_FEEDBACK.getPrompt())
-                            .build())
-                        .futureLearningStrategy(PropertyType.builder()
-                            .description(STUDY_FEEDBACK_PROPERTY_DESCRIPTION_STRATEGY.getPrompt())
-                            .build())
-                        .build())
-                    .build())
-                .build())
-            .build();
     }
 
     @Getter
     @Builder
     @AllArgsConstructor
-    private static class OpenAiRequest<T> {
+    private static class OpenAiRequest {
         @Builder.Default
         private String model = "gpt-4o";
         private List<OpenAiMessage> messages;
-        private OpenAiResponseFormat<T> response_format;
+        private Map response_format;
     }
 
     @Getter
@@ -122,77 +140,28 @@ public class OpenAiServiceImpl implements OpenAiService {
     }
 
     @Getter
-    @Builder
-    @AllArgsConstructor
-    private static class OpenAiResponseFormat<T> {
-        @Builder.Default
-        private String type = "json_schema";
-        private JsonSchema<T> json_schema;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    private static class JsonSchema<T> {
-        @Builder.Default
-        private String name = "study_feedback";
-        private Schema<T> schema;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    private static class Schema<T> {
-        @Builder.Default
-        private String type = "object";
-        private T properties;
-        @Builder.Default
-        private List<String> required = List.of("feedback", "futureLearningStrategy");
-        @Builder.Default
-        private Boolean additionalProperties = false;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    private static class StudyFeedbackProperties {
-        private PropertyType feedback;
-        private PropertyType futureLearningStrategy;
-    }
-
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    private static class PropertyType {
-        @Builder.Default
-        private String type = "string";
-        private String description;
-    }
-
-
-    @Getter
     @NoArgsConstructor
-    private static class OpenAiResponse<T> {
+    private static class OpenAiResponse {
         private String id;
         private String object;
         private Long created;
         private String model;
-        private List<OpenAiResponseChoice<T>> choices;
+        private List<OpenAiResponseChoice> choices;
     }
 
     @Getter
     @NoArgsConstructor
-    private static class OpenAiResponseChoice<T> {
+    private static class OpenAiResponseChoice {
         private Long index;
-        private OpenAiResponseMessage<T> message;
+        private OpenAiResponseMessage message;
         private String finish_reason;
     }
 
     @Getter
     @NoArgsConstructor
-    private static class OpenAiResponseMessage<T> {
+    private static class OpenAiResponseMessage {
         private String role;
-        private T content;
+        private String content;
         private String refusal;
     }
 }
