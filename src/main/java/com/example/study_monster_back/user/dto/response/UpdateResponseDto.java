be5ae7d1c38f058bb815960a6dc730c4819638a2
jangseungwon <jangseungwon9819@gmail.com>
2025-05-22 16:35:25 +0900

    feat/#36/mypage : 마이페이지 구현

---
+++ src/main/java/com/example/study_monster_back/user/dto/response/UpdateResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.study_monster_back.user.dto.response;
+
+import lombok.Data;
+
+@Data
+public class UpdateResponseDto {
+    private String email;
+    private String name;
+    private String nickname;
+}
