f380f9f25bf1ecd72254dc9e795f7615f193183c
Jiyoung Park <pjy2163@sookmyung.ac.kr>
2025-05-23 14:59:40 +0900

    [보드 로그인 연동] refactor : 보드쪽 로그인 연동 추가

--- src/main/java/com/example/study_monster_back/board/controller/BoardController.java
+++ src/main/java/com/example/study_monster_back/board/controller/BoardController.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.study_monster_back.board.dto.response.BoardResponse;
@@ -55,25 +57,25 @@ public class BoardController {
     @PostMapping
     @Operation(summary = "게시글 작성",
         description = "게시글을 작성하고, 해당 게시글의 해시태그도 소문자로 변환하여 저장합니다.")
-    public ResponseEntity<CreateBoardResponseDto> createBoard(@Valid @RequestBody CreateBoardRequestDto boardRequestDto) {
-        // TODO: 추후에 @AuthenticationPrincipal로 유저 정보 가져올 예정
-        CreateBoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto);
+    public ResponseEntity<CreateBoardResponseDto> createBoard(@Valid @RequestBody CreateBoardRequestDto boardRequestDto,
+    @AuthenticationPrincipal UserDetails userdetails) {
+        CreateBoardResponseDto boardResponseDto = boardService.createBoard(boardRequestDto, userdetails.getUsername());
         return ResponseEntity.ok(boardResponseDto);
     }
 
     @PutMapping("/{boardId}")
     @Operation(summary = "게시글 수정", description = "게시글과 게시글 태그를 수정합니다.")
-    public ResponseEntity<UpdateBoardResponseDto> updateBoard(@PathVariable Long boardId, @Valid @RequestBody UpdateBoardRequestDto boardRequestDto) {
-        // TODO: 추후에 @AuthenticationPrincipal로 유저 정보 가져와서 자신의 게시글인 경우에만 수정
-        UpdateBoardResponseDto updateBoardResponseDto = boardService.updateBoard(boardId,boardRequestDto);
+    public ResponseEntity<UpdateBoardResponseDto> updateBoard(@PathVariable Long boardId, @Valid @RequestBody UpdateBoardRequestDto boardRequestDto,
+                                                              @AuthenticationPrincipal UserDetails userdetails) {
+        UpdateBoardResponseDto updateBoardResponseDto = boardService.updateBoard(boardId,boardRequestDto, userdetails.getUsername());
         return ResponseEntity.ok(updateBoardResponseDto);
     }
 
     @DeleteMapping("/{boardId}")
     @Operation(summary = "게시글 삭제", description = "게시글을 삭제하면서 게시글과 연관된 댓글, 좋아요, 피드백, 게시글 태그를 함께 삭제합니다.")
-    public ResponseEntity<String> deleteBoard(@PathVariable Long boardId) {
-        // TODO: 추후에 @AuthenticationPrincipal로 유저 정보 가져와서 자신의 게시글일 경우에만 삭제
-        boardService.deleteBoard(boardId);
+    public ResponseEntity<String> deleteBoard(@PathVariable Long boardId, @AuthenticationPrincipal UserDetails userdetails) {
+
+        boardService.deleteBoard(boardId, userdetails.getUsername());
         return ResponseEntity.ok("게시글을 정상적으로 삭제했습니다.");
     }
 
